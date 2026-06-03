77f239b1c6cb89bc71344880e9e05bbf2a4c6656
sheylis11 <sheylisniki@gmail.com>
2026-06-03 20:18:29 +0000

    taller rick and morty

--- 03-06/03-06/app.js
+++ 03-06/03-06/app.js
@@ -105,3 +105,38 @@ obtenerPersonajes();
 soloNombres();
 
 obtenerPagina(2);
+
+
+const url =
+  "https://rickandmortyapi.com/api/character";
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+async function obtenerPersonajes() {
+  try {
+
+    console.log("Consultando API...");
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers,
+    });
+
+    const data = await response.json();
+
+    console.log("Respuesta de la API:");
+    console.log(data);
+
+    console.table(data);
+
+  } catch (error) {
+
+    console.log("Error:");
+    console.log(error);
+
+  }
+}
+
+obtenerPersonajes();
